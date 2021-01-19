87c7be4e5ac5e74909c6b58aeb88adb731963041
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-01-19 16:42:48 +0400

    Created ProjectState class, made to rerender project list whenever adds new project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,56 @@
+// Project Type
+enum  ProjectStatus{Active, Finished}
+
+type Listener = (items: Project[]) => void;
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
+// Project State Managment
+class ProjectState {
+  private projects: Project[] = [];
+  private listeners: Listener[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Listener) {
+    this.listeners.push(listenerFn);
+  }
+
+  addProject(title: string, description: string, numberOfPeople: number) {
+    const newProject = new Project (
+      Math.random().toString(),
+      title,
+      description,
+      numberOfPeople,
+      ProjectStatus.Active 
+    );
+    this.projects.push(newProject);
+    console.log(this.projects)
+    for (let listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 interface Validable {
   value: string | number;
   required?: boolean;
@@ -47,7 +100,69 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
-// Code goes here!
+// ProjectList Class
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  assignedProjects: Project[] = [];
+
+  constructor(private type: "active" | "finished") {
+    // we guaranteed to TS that, we fetch beyond will not be null(!) and will be this type(as HTMLTemplateElement[type casting])
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    // Arrow syntax automatically binds to the surounding code's context under the hood.
+    // Basically in the Arrow function "this" always represents the object in which
+    // the arrow function is defined.
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter(prj=>{
+        if(this.type === "active" ){
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status = ProjectStatus.Finished;
+      })
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+
+    this.attach();
+    this.renderContent();
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = prjItem.title;
+      listEl.appendChild(listItem);
+    }
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -105,7 +220,7 @@ class ProjectInput {
     };
 
     if (
-      !validate(titleValidate) || 
+      !validate(titleValidate) ||
       !validate(descriptionValidate) ||
       !validate(peopleValidate)
     ) {
@@ -123,7 +238,8 @@ class ProjectInput {
     const userInput = this.gatherUserInputs();
     if (Array.isArray(userInput)) {
       const [title, description, people] = userInput;
-      console.log(title, description, people);
+      // console.log(title, description, people);
+      projectState.addProject(title, description, people);
     }
   }
 
@@ -137,3 +253,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");

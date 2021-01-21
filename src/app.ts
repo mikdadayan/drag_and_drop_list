528d8aafd3fefd9d535938fc7983cb78ff2abcf2
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-01-21 12:30:29 +0400

    clean up

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 // Drag & Drop
+
+// Dragable interfave helps us to write dragable components or draggable classes in a uniform way.
+// So if we add more and more classes which should be dragable we always will have this methods
 interface Draggable {
   dragStartHandler(event: DragEvent): void;
   dragEndHandler(event: DragEvent): void;

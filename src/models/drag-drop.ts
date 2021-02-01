f6fdcab11e4e458197272199d2a752b298e6ac65
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-02-01 09:37:46 +0400

    clean up

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,5 +1,4 @@
 // Drag & Drop
-
 // Dragable interfave helps us to write dragable components or draggable classes in a uniform way.
 // So if we add more and more classes which should be dragable we always will have this methods
 export interface Draggable {
